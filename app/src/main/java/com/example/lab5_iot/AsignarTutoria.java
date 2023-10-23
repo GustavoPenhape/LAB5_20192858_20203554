a00c9b5b395e6118e0aaeafbee00046fcbaf50c0
Gustavo <a20192858@pucp.edu.pe>
2023-10-23 16:10:16 -0500

    commit

--- app/src/main/java/com/example/lab5_iot/AsignarTutoria.java
+++ app/src/main/java/com/example/lab5_iot/AsignarTutoria.java
@@ -1,6 +1,7 @@
 package com.example.lab5_iot;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,9 +11,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.lab5_iot.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AsignarTutoria extends AppCompatActivity {
 
@@ -36,28 +44,41 @@ public class AsignarTutoria extends AppCompatActivity {
             public void onClick(View view) {
                 String tutorEmployeeCode = editTutorCode.getText().toString();
                 String employeeIdToAssign = editEmployeeId.getText().toString();
-                enviarSolicitudPost(tutorEmployeeCode, employeeIdToAssign);
+                int tutorCodeAsNumber = Integer.parseInt(tutorEmployeeCode);
+                int employeeIdAsNumber = Integer.parseInt(employeeIdToAssign);
+                enviarSolicitudPost(tutorCodeAsNumber, employeeIdAsNumber);
             }
         });
     }
 
-    private void enviarSolicitudPost(String tutorCode, String employeeId) {
-        String url = "http://localhost:3000/asignartutoria"; // Reemplaza con la URL correcta de tu servidor Node.js
+    private void enviarSolicitudPost(Integer tutorCode, Integer employeeId) {
+        String url = "http://127.0.0.1:3000/asignartutoria";
 
-        // Crear una solicitud POST con los datos del tutor y el empleado
-        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                // Procesa la respuesta del servidor aquí
-                Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                // Manejar errores de la solicitud
-                Toast.makeText(getApplicationContext(), "Error en la solicitud", Toast.LENGTH_LONG).show();
-            }
-        });
+        // Crear un objeto JSON con los parámetros
+        JSONObject jsonParams = new JSONObject();
+        try {
+            jsonParams.put("tutorEmployeeCode", tutorCode);
+            jsonParams.put("employeeIdToAssign", employeeId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        // Crear una solicitud POST
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, url, jsonParams,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Procesa la respuesta del servidor aquí
+                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Manejar errores de la solicitud
+                        Toast.makeText(getApplicationContext(), "Error en la solicitud", Toast.LENGTH_LONG).show();
+                    }
+                });
 
         // Agregar la solicitud a la cola de solicitudes
         RequestQueue requestQueue = Volley.newRequestQueue(this);
