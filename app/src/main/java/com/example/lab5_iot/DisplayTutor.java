8a390abc3f7e6f3ac9124dc5119c7dc3da447546
Gustavo <a20192858@pucp.edu.pe>
2023-10-23 13:32:31 -0500

    cambios en androidss

--- app/src/main/java/com/example/lab5_iot/DisplayTutor.java
+++ app/src/main/java/com/example/lab5_iot/DisplayTutor.java
@@ -2,7 +2,10 @@ package com.example.lab5_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DisplayTutor extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class DisplayTutor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_tutor);
+
+        Button descargarListaButton = findViewById(R.id.DescargarLista);
+        Button buscarTrabajadorButton = findViewById(R.id.BuscarTrabajador);
+        Button asignarTutoriaButton = findViewById(R.id.asignarTutoria);
+
+        asignarTutoriaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Crear un Intent para abrir la actividad DisplayTutor
+                Intent intent = new Intent(DisplayTutor.this, AsignarTutoria.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
